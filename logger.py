29c7ed3b102d9a4b24ab74ac49fbbe4e445df7e5
Sleep e <80415229+3V5@users.noreply.github.com>
2021-05-30 20:33:43 -0700

    logger.py
    it work now

--- logger.py
+++ logger.py
@@ -13,16 +13,17 @@ client = commands.Bot(command_prefix=prefix)
 
 @client.event
 async def on_message(message):
-    if DiscordID in message.content:
-        msgcontent = str(message.content)
-        msgguild = str(message.guild)
-        webhook = DiscordWebhook(url=Webhook, username="Soy Ping Logger")
-        embed = DiscordEmbed(title='New Ping!', description='Caused By: '+str(message.author), color=242424)
-        embed.set_author(name='Soy Util', url='https://sex.com', icon_url=Icon)
-        embed.set_footer(text='New Ping!')
-        embed.set_timestamp()
-        embed.add_embed_field(name="Message Details: ", value=f"Content: {message.content} \nGuild: {message.guild}\nLink: [click here]({message.jump_url})")
-        webhook.add_embed(embed)
-        response = webhook.execute()
+    async for message in message.channel.history(limit=1):
+        if DiscordID in message.content:
+            msgcontent = str(message.content)
+            msgguild = str(message.guild)
+            webhook = DiscordWebhook(url=Webhook, username="Soy Ping Logger")
+            embed = DiscordEmbed(title='New Ping!', description='Caused By: '+str(message.author), color=242424)
+            embed.set_author(name='Soy Util', url='https://sex.com', icon_url=Icon)
+            embed.set_footer(text='New Ping!')
+            embed.set_timestamp()
+            embed.add_embed_field(name="Message Details: ", value=f"Content: {message.content} \nGuild: {message.guild}\nLink: [click here]({message.jump_url})")
+            webhook.add_embed(embed)
+            response = webhook.execute()
 
 client.run(token, bot=False)
